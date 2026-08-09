b9d6f7ebae735614e509cce9001f2a70b529a5d1
Sergey Mironov <sergeymironov@protonmail.com>
2026-08-09 18:25:04 +0300

    0.5.0

--- src/js/bootstrap-sheet.ts
+++ src/js/bootstrap-sheet.ts
@@ -32,7 +32,7 @@ interface SheetHandlers {
 
 /**
  * @class BootstrapSheet - A touch-friendly bottom sheet component for Bootstrap 5
- * @version 0.4.0
+ * @version 0.5.0
  * @author Sergey Mironov <sergeymironov@protonmail.com>
  * @license MIT (https://github.com/mironovsergey/bootstrap-sheet/blob/main/LICENSE)
  */
